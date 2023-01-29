571ede4f576ee8bbf73b130af4d7a7d152dce986
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-01-28 21:04:34 -0800

    Fixed logging bug

--- utils/logging.py
+++ utils/logging.py
@@ -3,13 +3,16 @@ import uvicorn.logging
 
 FORMAT = "%(levelprefix)s %(message)s"
 _formatter = uvicorn.logging.DefaultFormatter(FORMAT)
+_logger = None
 
 
 def getMainLogger():
-    logger = logging.getLogger("main_logger")
-    ch = logging.StreamHandler()
-    ch.setFormatter(_formatter)
-    logger.addHandler(ch)
-    logger.setLevel(logging.DEBUG)
+    global _logger
+    if _logger is None:
+        _logger = logging.getLogger("main_logger")
+        ch = logging.StreamHandler()
+        ch.setFormatter(_formatter)
+        _logger.addHandler(ch)
+        _logger.setLevel(logging.DEBUG)
 
-    return logger
+    return _logger
